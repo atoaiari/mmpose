8e6fe7e5e065c5ecf56168d747c2e17a2cfaaf2c
jinsheng <jinsheng@sensetime.com>
2021-06-21 18:00:41 +0800

    move adaptive wingloss to heatmap_loss

--- mmpose/models/losses/__init__.py
+++ mmpose/models/losses/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .classfication_loss import BCELoss
+from .heatmap_loss import AdaptiveWingLoss
 from .mesh_loss import GANLoss, MeshLoss
 from .mse_loss import JointsMSELoss, JointsOHKMMSELoss
 from .multi_loss_factory import AELoss, HeatmapLoss, MultiLossFactory
-from .regression_loss import (AdaptiveWingLoss, BoneLoss, L1Loss, MPJPELoss,
-                              MSELoss, SemiSupervisionLoss, SmoothL1Loss,
-                              WingLoss)
+from .regression_loss import (BoneLoss, L1Loss, MPJPELoss, MSELoss,
+                              SemiSupervisionLoss, SmoothL1Loss, WingLoss)
 
 __all__ = [
     'JointsMSELoss', 'JointsOHKMMSELoss', 'HeatmapLoss', 'AELoss',
